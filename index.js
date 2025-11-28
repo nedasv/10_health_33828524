8fa291dae50cf744bebaee76fab1a296aac149da
nedasv <nedasvauspas@gmail.com>
2025-11-28 20:04:25 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,64 @@
+// Import express and ejs
+var express = require ('express')
+var ejs = require('ejs')
+const path = require('path')
+var session = require ('express-session')
+const expressSanitizer = require('express-sanitizer');
+
+// Set up .env file
+require('dotenv').config();
+
+// MySQL setup
+var mysql = require('mysql2');
+
+// Create the express application object
+const app = express()
+const port = 8000
+
+// Tell Express that we want to use EJS as the templating engine
+app.set('view engine', 'ejs')
+
+// Set up the body parser 
+app.use(express.urlencoded({ extended: true }))
+
+// Set up public folder (for css and static js)
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Create a session
+app.use(session({
+    secret: 'somerandomstuff',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        expires: 600000
+    }
+}))
+
+// Create an input sanitizer
+app.use(expressSanitizer());
+
+// Define our application-specific data
+// app.locals.shopData = {shopName: "Bertie's Books"}
+
+// Define the database connection pool
+// const db = mysql.createPool({
+//     host: process.env.BB_HOST,
+//     user:  process.env.BB_USER,
+//     password:  process.env.BB_PASSWORD,
+//     database: process.env.BB_DATABASE,
+//     waitForConnections: true,
+//     connectionLimit: 10,
+//     queueLimit: 0,
+// });
+// global.db = db;
+
+// Load the route handlers
+const mainRoutes = require("./routes/main")
+app.use('/', mainRoutes)
+
+// // Load the route handlers for /users
+// const usersRoutes = require('./routes/users')
+// app.use('/users', usersRoutes)
+
+// Start the web app listening
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
---
+++ routes/main.js
@@ -0,0 +1,11 @@
+// Create a new router
+const express = require("express")
+const router = express.Router()
+
+// Handle our routes
+router.get('/',function(req, res, next){
+    res.render('index.ejs')
+});
+
+// Export the router object so index.js can access it
+module.exports = router
