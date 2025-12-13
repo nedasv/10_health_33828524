27bd4a723d03e2e25bab642c6701287a969b5089
nedasv <nedasvauspas@gmail.com>
2025-12-13 13:41:04 +0000

    register changes

--- routes/main.js
+++ routes/main.js
@@ -11,40 +11,65 @@ router.get('/', function(req, res, next){
     res.render('index.ejs')
 });
 
+router.get('/about', function(req, res, next) {
+    res.render('about.ejs');
+});
+
 router.get('/register', function(req, res, next){
     res.render('register.ejs')
 });
 
 router.post('/registered', 
     [
-        check('email').isEmail(), 
-        check('username').isLength({ min: 5, max: 20}), 
-        check('password').isLength({min: 8})
-    ], function (req, res, next) 
-{
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        res.render('./register')
-    }
-    else { 
+        check('email').isEmail().withMessage("Enter valid email"), 
+        check('username').isLength({ min: 5, max: 20}).withMessage('Username must be between 5-20 characters'), 
+        check('password')
+            .isLength({min: 8}).withMessage('Password must be at least 8 characters')
+            .matches(/[a-z]/).withMessage('Password must have at least one lowercase letter')
+            .matches(/[A-Z]/).withMessage('Password must have at least one uppercase letter')
+            .matches(/[0-9]/).withMessage('Password must have at least one number')
+            .matches(/[!@#$%^&*(),.?":{}|<>]/).withMessage('Password must have at least one special character'),
+        check('first').notEmpty().withMessage('Must have first name'),
+        check('last').notEmpty().withMessage('Must have last name'),
+        check('confirmPassword').custom((value, { req }) => {
+            if (value !== req.body.password) {
+                throw new Error('Passwords do not match');
+            }
+            return true;    
+        })
+    ], 
+    function (req, res, next) {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            // Renders the page but with error msg
+            return res.render('register.ejs', {errors: errors.array()})
+        } 
         // saving data in database
         const plainPassword = req.body.password
         bcrypt.hash(plainPassword, saltRounds, function(err, hashedPassword) {
+            if (err) {
+                return next(err);
+            }
+
             // Store hashed password in your database.
             let sqlquery = "INSERT INTO users (username, first_name, last_name, email, password_hash) VALUES (?,?,?,?,?)"
             // execute sql query
             let newrecord = [req.sanitize(req.body.username), req.sanitize(req.body.first), req.sanitize(req.body.last), req.body.email, hashedPassword]
             db.query(sqlquery, newrecord, (err, result) => {
                 if (err) {
-                    next(err)
+                    // If database returns error of there already being a duplicate entry
+                    if (err.code == 'ER_DUP_ENTRY') {
+                        // Renders the page but with error msg
+                        return res.render('register.ejs', {errors: [{ msg: 'Username or email already exists' }]})
+                    }
+                    return next(err)
                 }
-                else
-                    result = 'Hello '+ req.sanitize(req.body.first) + ' '+ req.sanitize(req.body.last) +' you are now registered!  We will send an email to you at ' + req.body.email + 'Your password is: '+ req.body.password +' and your hashed password is: '+ hashedPassword
-                    res.send(result)
+                res.redirect('./login');
             })
-        })         
-    }                                                                 
-}); 
+        })                                                                         
+    }
+); 
 
 router.get('/login', function(req, res, next){
     res.render('login.ejs')
